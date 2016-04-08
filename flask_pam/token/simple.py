beef9b73861ee6a349f7bc76bb58d8bed935f903
Kacper Kołodziej <kacper@kolodziej.in>
2016-04-08 14:11:47 +0200

    Py's docstring for Simple class

--- flask_pam/token/simple.py
+++ flask_pam/token/simple.py
@@ -3,7 +3,8 @@ from hashlib import sha256
 from token import Token
 
 class Simple(Token):
-    
+    """Simple token implementation. It's not safe. Only for testing purposes!"""
+
     def generate(self):
         return sha256(self.username).hexdigest()
 
