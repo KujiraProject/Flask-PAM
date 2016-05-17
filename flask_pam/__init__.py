c2eb3e5e3a8067e7d796b30d54d352ca05f32db7
Kacper Kołodziej <kacper@kolodziej.in>
2016-05-17 09:41:26 +0200

    landscape: fix error name

--- flask_pam/__init__.py
+++ flask_pam/__init__.py
@@ -1,3 +1,3 @@
-from auth import Auth  # pylint: disable=unsed-import
+from auth import Auth  # pylint: disable=unused-import
 import token_storage  # pylint: disable=unused-import
 import token  # pylint: disable=unused-import
