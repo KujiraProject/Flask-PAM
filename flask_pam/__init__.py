cef832ed7cc7b146f8876c01bb1f84c040a64d25
Kacper Kołodziej <kacper@kolodziej.in>
2016-05-17 09:36:12 +0200

    landscape: inline diable F401 in __init__.py

--- flask_pam/__init__.py
+++ flask_pam/__init__.py
@@ -1,3 +1,3 @@
-from auth import Auth
-import token_storage
-import token
+from auth import Auth  # pylint: disable=F401
+import token_storage  # pylint: disable=F401
+import token  # pylint: disable=F401
