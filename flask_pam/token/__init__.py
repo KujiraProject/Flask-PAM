b6cdc4002caf5654fbfe4797840476c9af380071
Kacper Kołodziej <kacper@kolodziej.in>
2016-05-23 16:45:29 +0200

    ignore unused warning from landscape

--- flask_pam/token/__init__.py
+++ flask_pam/token/__init__.py
@@ -1,3 +1,3 @@
-from token import Token
-from simple import Simple
-from jwt import JWT
+from token import Token  # pylint: ignore=unused-import
+from simple import Simple  # pylint: ignore=unused-import
+from jwt import JWT  # pylint: ignore=unused-import
